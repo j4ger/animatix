64bd90949f28ed73b41759879555b7e59f36e7d1
j4ger <xiayuxuan@live.com>
2026-04-04 00:40:13 +0800

    fix wayland deps

--- flake.nix
+++ flake.nix
@@ -16,16 +16,26 @@
         };
       in
       {
-        devShells.default = with pkgs; mkShell {
+        devShells.default = with pkgs; mkShell rec {
           buildInputs = [
             ffmpeg_7
             pkg-config
             rustPlatform.bindgenHook
 
+	    xorg.libX11
+	    xorg.libXcursor
+	    xorg.libXrandr
+	    xorg.libXi
+	    xorg.libxcb
+	    libxkbcommon
+	    vulkan-loader
+	    wayland
+
             rust-bin.stable.latest.default
           ];
 
           shellHook = ''
+		export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
           '';
         };
       }
